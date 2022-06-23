e6a4aaa04c16701144acc87047d907945f40bd81
gilbert <annatibekina3@gmail.com>
2022-06-23 18:05:43 +0300

    Initial Commit

---
+++ LoginPassword/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginPassword
+//
+//  Created by Анна Тибекина on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
